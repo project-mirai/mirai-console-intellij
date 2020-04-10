d1846126728035b9358ee1823c2750e00c2b9a85
Him188 <Him188@mamoe.net>
2020-04-10 09:30:02 +0800

    Update java template

--- src/main/java/net/mamoe/mirai/intellij/builder/ContentBuilder.kt
+++ src/main/java/net/mamoe/mirai/intellij/builder/ContentBuilder.kt
@@ -207,74 +207,86 @@ object Template {
 
     @Language("java")
     val pluginBaseJava: String = """
-        package PACKAGE;
+package PACKAGE;
 
-        import net.mamoe.mirai.console.plugins.PluginBase;
+import kotlinx.coroutines.Job;
+import net.mamoe.mirai.console.plugins.PluginBase;
+import net.mamoe.mirai.contact.Contact;
+import net.mamoe.mirai.message.GroupMessage;
+import net.mamoe.mirai.message.MessageReceipt;
+import net.mamoe.mirai.message.data.*;
 
-        class ExamplePluginMain extends PluginBase {
-            
-            public void onLoad(){
-                Events.subscribeAlways(GroupMessage.class, (GroupMessage event) -> {
+import java.io.File;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
-                    if (event.getMessage().contains("reply")) {
-                        // 引用回复
-                        final QuoteReplyToSend quote = MessageUtils.quote(event.getMessage(), event.getSender());
-                        event.getGroup().sendMessage(quote.plus("引用回复"));
+class MAIN_CLASS_NAME extends PluginBase {
 
-                    } else if (event.getMessage().contains("at")) {
-                        // at
-                        event.getGroup().sendMessage(new At(event.getSender()));
+    public void onLoad() {
+        getLogger().info("Plugin loaded!");
+    }
 
-                    } else if (event.getMessage().contains("permission")) {
-                        // 成员权限
-                        event.getGroup().sendMessage(event.getPermission().toString());
+    public void onEnable() {
+        getLogger().info("Plugin enabled!");
 
-                    } else if (event.getMessage().contains("mixed")) {
-                        // 复合消息, 通过 .plus 连接两个消息
-                        event.getGroup().sendMessage(
-                                MessageUtils.newImage("{01E9451B-70ED-EAE3-B37C-101F1EEBF5B5}.png") // 演示图片, 可能已过期
-                                        .plus("Hello") // 文本消息
-                                        .plus(new At(event.getSender())) // at 群成员
-                                        .plus(AtAll.INSTANCE) // at 全体成员
-                        );
+        this.getEventListener().subscribeAlways(GroupMessage.class, (GroupMessage event) -> {
+            String content = event.getMessage().contentToString();
+            if (content.contains("reply")) {
+                // 引用回复
+                final QuoteReply quote = MessageUtils.quote(event.getMessage());
+                event.getGroup().sendMessage(quote.plus("引用回复"));
 
-                    } else if (event.getMessage().contains("recall1")) {
-                        event.getGroup().sendMessage("你看不到这条消息").recall();
-                        // 发送消息马上就撤回. 因速度太快, 客户端将看不到这个消息.
+            } else if (content.contains("at")) {
+                // at
+                event.getGroup().sendMessage(new At(event.getSender()));
 
-                    } else if (event.getMessage().contains("recall2")) {
-                        final Job job = event.getGroup().sendMessage("3秒后撤回").recallIn(3000);
+            } else if (content.contains("permission")) {
+                // 成员权限
+                event.getGroup().sendMessage(event.getPermission().toString());
 
-                        // job.cancel(new CancellationException()); // 可取消这个任务
+            } else if (content.contains("mixed")) {
+                // 复合消息, 通过 .plus 连接两个消息
+                event.getGroup().sendMessage(
+                        MessageUtils.newImage("{01E9451B-70ED-EAE3-B37C-101F1EEBF5B5}.png") // 演示图片, 可能已过期
+                                .plus("Hello") // 文本消息
+                                .plus(new At(event.getSender())) // at 群成员
+                                .plus(AtAll.INSTANCE) // at 全体成员
+                );
 
-                    } else if (event.getMessage().contains("上传图片")) {
-                        File file = new File("myImage.jpg");
-                        if (file.exists()) {
-                            final Image image = event.getGroup().uploadImage(new File("myImage.jpg"));
-                            // 上传一个图片并得到 Image 类型的 Message
+            } else if (content.contains("recall1")) {
+                event.getGroup().sendMessage("你看不到这条消息").recall();
+                // 发送消息马上就撤回. 因速度太快, 客户端将看不到这个消息.
 
-                            final String imageId = image.getImageId(); // 可以拿到 ID
-                            final Image fromId = MessageUtils.newImage(imageId); // ID 转换得到 Image
+            } else if (content.contains("recall2")) {
+                final Job job = event.getGroup().sendMessage("3秒后撤回").recall(3000);
 
-                            event.getGroup().sendMessage(image); // 发送图片
-                        }
+                // job.cancel(new CancellationException()); // 可取消这个任务
 
-                    } else if (event.getMessage().contains("friend")) {
-                        final Future<MessageReceipt<? extends Contact>> future = event.getSender().sendMessageAsync("Async send"); // 异步发送
-                        try {
-                            future.get();
-                        } catch (InterruptedException | ExecutionException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
+            } else if (content.contains("上传图片")) {
+                File file = new File("myImage.jpg");
+                if (file.exists()) {
+                    final Image image = event.getGroup().uploadImage(new File("myImage.jpg"));
+                    // 上传一个图片并得到 Image 类型的 Message
+                    event.getGroup().sendMessage(image); // 发送图片
+
+                    final String imageId = image.getImageId(); // 可以拿到 ID
+                    final Image fromId = MessageUtils.newImage(imageId); // ID 转换得到 Image
+
+                    event.getGroup().sendMessage(fromId);
+                }
+
+            } else if (content.contains("friend")) {
+                final Future<MessageReceipt<Contact>> future = event.getSender().sendMessageAsync("Async send"); // 异步发送
+                try {
+                    future.get();
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
             }
-            
-            public void onEnable(){
-                logger.info("Plugin loaded!");
-            }
-            
-        }        
+        });
+    }
+
+}          
     """.trimIndent()
 
     @Language("java")
